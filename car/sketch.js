2b5fd156a7637da280a39eadb35b30d49fc68a85
ijazhussain423 <159792798+ijazhussain423@users.noreply.github.com>
2024-06-12 08:18:02 -0700

    Car
    https://editor.p5js.org/ijazyousaf423/sketches/bcZDG1gHI

---
+++ car/sketch.js
@@ -0,0 +1,42 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  
+  // draw body of the car
+fill("green")
+  rect(51,200.5,301,50);
+ fill("lightgreen")
+  rect(100.1,150.1,200,50)
+  
+  //right window 
+  fill(0)
+  rect
+  (225,155,50,40)
+  //left window
+  fill(0)
+  rect
+  (125,155,50,40)
+ // wheel 
+fill("black")
+  ellipse(100,250,70,70);
+  // front wheel
+  ellipse (300,250,70,70);
+  // back wheel
+  
+  //roof
+  fill("0")
+  
+  rect
+  
+  (100,120,200,30,200,142)
+  
+  // head lights
+  fill("yellow")
+  rect(52,200,20,18);
+  rect(331,200,20,18);
+  
+  
+}
