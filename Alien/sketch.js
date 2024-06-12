b0f2f0048245fb4c34b623f8a67da663c1e7bcc2
ijazhussain423 <159792798+ijazhussain423@users.noreply.github.com>
2024-06-12 08:19:56 -0700

    Alien
    https://editor.p5js.org/ijazyousaf423/sketches/T-Smnbzju

---
+++ Alien/sketch.js
@@ -0,0 +1,32 @@
+function setup(){createCanvas(500,500);
+background(250);
+}
+function draw(){
+  //antena
+  fill(232,71,232);
+  line(255,95,255,65);
+   rect(255,65,10);
+   //body
+   fill(114,101,199);
+  rect(205,200,100,100);
+ //head
+ fill(93,255,131);
+ circle(255,150,110);
+ //eyes
+ fill(2,2,2);
+  square(230,140,20,40);
+  square(265,140,20,40);
+  //feet
+  fill(93,255,131);
+  square(205,300,35,50);
+  square(270,300,35,50);
+
+    //arms
+    fill(93,255,131);
+  ellipse(205,240,20,80);
+  ellipse(305,240,20,80);
+  //triangle
+  fill(255,98,242);
+  rect(217,225,75,50,200,200);
+
+}
